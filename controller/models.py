3a1adf49f5f7d4dfcd1f0f8b216b0902d0619fbb
Aarav Shukla <aarav.shukla105@gmail.com>
2025-02-19 22:37:19 +0530

    Created database schema for users, subjects, quizzes, and scores

---
+++ controller/models.py
@@ -0,0 +1,79 @@
+from controller.database import db
+
+class User(db.Model):
+
+    # __tablename__ = 'user'
+
+    user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_email = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(50), nullable=False)
+    user_name = db.Column(db.String(50), nullable=False)
+    roles = db.relationship('Role', secondary='user_role', backref='users', lazy=True)
+
+class Role(db.Model):
+
+    # __tablename__ = 'user_role'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(50), nullable=False)
+
+class UserRole(db.Model):
+
+    # __tablename__ = 'user_role'
+
+    id = db.Column(db.Integer, primary_key = True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
+    role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
+
+class Subject(db.Model):
+
+    #__tablename__ = 'subject'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    description = db.Column(db.String(50))
+    chapters = db.relationship('Chapter', backref='subject', lazy=True)
+
+class Chapter(db.Model):
+
+    #__tablename__ = 'chapter'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(150), nullable=False)
+    description = db.Column(db.String(500))
+    subject_id = db.Column(db.Integer, db.ForeignKey('subject.id'), nullable=False)
+    quizzes = db.relationship('Quiz', backref='chapter', lazy=True)
+
+class Quiz(db.Model):
+
+    #__tablename__ = 'quiz'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    chapter_id = db.Column(db.Integer, db.ForeignKey('chapter.id'), nullable=False)
+    date_of_quiz = db.Column(db.DateTime, nullable=False)
+    time_duration = db.Column(db.String(10), nullable=False)
+    remarks = db.Column(db.String(500))
+    questions = db.relationship('Question', backref='quiz', lazy=True)
+
+class Question(db.Model):
+
+    #__tablename__ = 'question'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    quiz_id = db.Column(db.Integer, db.ForeignKey('quiz.id'), nullable=False)
+    question_statement = db.Column(db.String(500), nullable=False)
+    option1 = db.Column(db.String(300), nullable=False)
+    option2 = db.Column(db.String(300), nullable=False)
+    option3 = db.Column(db.String(300), nullable=False)
+    option4 = db.Column(db.String(300), nullable=False)
+    correct_option = db.Column(db.Integer, nullable=False)
+
+class Score(db.Model):
+
+    #__tablename__ = 'score'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    quiz_id = db.Column(db.Integer, db.ForeignKey('quiz.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
+    time_stamp_of_attempt = db.Column(db.DateTime, nullable=False)
+    total_scored = db.Column(db.Integer, nullable=False)
